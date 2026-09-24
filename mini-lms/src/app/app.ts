c40b141d34a7b49c5ac0fb822c344d74aa4d2ddd
iTzDraxord_06 <u24239989@utp.edu.pe>
2026-09-24 14:27:30 +0000

    angular

---
+++ mini-lms/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  imports: [RouterOutlet],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  titulo = 'Mini-LMS · Semana 7';
+  contenido = 'Mundo Hola xd';
+}
